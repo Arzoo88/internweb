9156cd8bf77236da2812f06e12df36e3a6812ca6
Arzoo Rai <129486623+Arzoo88@users.noreply.github.com>
2023-10-12 00:00:03 +0530

    Add files via upload

---
+++ fullwebsite/connect.php
@@ -0,0 +1,6 @@
+<?php
+   $Your Name= $_POST ['Your Name'];
+   $Phone Number= $_POST ['Phone Number'];
+   $Your Email Address= $_POST ['Your Email Address'];
+   $Car model and brand= $_POST ['Car model and brand'];
+?>
